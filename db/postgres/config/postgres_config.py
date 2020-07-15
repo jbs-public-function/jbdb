04a9de6bc85f238bdade81f03aac76f9abcbce70
JB <54007352+jbs-public-function@users.noreply.github.com>
2020-07-14 20:52:22 -0500

    Feat/remove alembic dependency (#3)
    * removing alembic for now. This is more useful as a package. Need to consider consequences of using orms if it's just for the sql alchemy pooling
    
    * removing alembic for now. This is more useful as a package. Need to consider consequences of using orms if it's just for the sql alchemy pooling
    
    Co-authored-by: jbagrible <james@agrible.com>

--- db/postgres/config/postgres_config.py
+++ db/postgres/config/postgres_config.py
@@ -3,4 +3,4 @@ from db.core.db_config_base import DBConfigBase
 
 class PostgresConfig(DBConfigBase):
     __slots__ = DBConfigBase.__slots__ + ['user_name', 'password', 'host', 'port', 'db_name']
-    db_uri = '{driver}://{user_name}:{password}@{host}:{port}/{db_name}'
+    db_uri = DBConfigBase.db_uri + '://{user_name}:{password}@{host}:{port}/{db_name}'
